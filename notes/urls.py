250803c046bb9d6d17e98dc8c5988ff4201c3779
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-03-04 13:33:04 +0100

    refactor: change NoteViewSet route from /notes/ to root

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import NoteViewSet
 
 router = routers.DefaultRouter()
-router.register(r'notes', NoteViewSet)
+router.register(r'', NoteViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
