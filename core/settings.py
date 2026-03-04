10d9b0c21b7deef7241ed434029ddcf79a0b71c4
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-03-04 13:32:38 +0100

    settings: update CORS and CSRF origins to port 4200

--- core/settings.py
+++ core/settings.py
@@ -56,12 +56,12 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5500',
-    'http://localhost:5500',
+    'http://localhost:4200',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
-    'http://localhost:5500',
+    'http://localhost:4200',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
